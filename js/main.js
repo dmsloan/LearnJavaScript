5ce3c693c39127d50daa7e8e6e6460db0455b79a
dmsloan from Dell Latitude <dsloan@ibe-inc.com>
2018-08-06 08:41:05 -0700

    added event listeners

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
+var ourList = document.getElementById("our-list");
+var ourButton = document.getElementById("our-button");
+var ourHeadline = document.getElementById("our-headline");
 var listItems = document.getElementById("our-list").getElementsByTagName("li");
 
-for (let i = 0; index < listItems.length; i++) {
-    listItems[i].innerHTML = "Hello World" ;
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener("click", activateItem);
+}
+
+function activateItem() {
+    ourHeadline.innerHTML = this.innerHTML;
+}
+
+ourButton.addEventListener("click", createNewItem);
+
+function createNewItem() {
+    ourList.innerHTML += "<li>Someting new</li>";
 }
