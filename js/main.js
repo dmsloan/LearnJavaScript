3e6efac53991952d0f91d2fc1bfa0a5997a48463
dmsloan from Dell Latitude <dsloan@ibe-inc.com>
2018-08-06 23:53:15 -0700

    completed video.html and attached main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
+var newItemCounter = 1
 var ourList = document.getElementById("our-list");
 var ourButton = document.getElementById("our-button");
 var ourHeadline = document.getElementById("our-headline");
 var listItems = document.getElementById("our-list").getElementsByTagName("li");
 
-for (let i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener("click", activateItem);
-}
+// the following for list works on load but not when additional list items are added.
+//for (let i = 0; i < listItems.length; i++) {
+//    listItems[i].addEventListener("click", activateItem);
+//}
 
-function activateItem() {
-    ourHeadline.innerHTML = this.innerHTML;
+ourList.addEventListener("click", activateItem);
+
+function activateItem(e) {
+    if (e.target.nodeName == "LI") {
+        ourHeadline.innerHTML = e.target.innerHTML;
+        for (let i = 0; i < e.target.parentNode.children.length; i++) {
+            e.target.parentNode.children[i].classList.remove("active");
+        }
+        e.target.classList.add("active");
+    }
 }
 
 ourButton.addEventListener("click", createNewItem);
 
 function createNewItem() {
-    ourList.innerHTML += "<li>Someting new</li>";
+    ourList.innerHTML += "<li>Someting new " + newItemCounter + "</li>";
+    newItemCounter++;
 }
